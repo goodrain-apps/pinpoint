b2ae74d1e8d599c053dc0511fafa0844f8cc1fa9
zhouyq <zhouyq@goodrain.com>
2017-12-23 10:15:40 +0800

    [FIX] 调整启动脚本

--- collector/docker-entrypoint.sh
+++ collector/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-set -x
+
+[[ $DEBUG ]] && set -x
 
 CLUSTER_ENABLE=${CLUSTER_ENABLE:-false}
 CLUSTER_ZOOKEEPER_ADDRESS=${CLUSTER_ZOOKEEPER_ADDRESS:-127.0.0.1}
@@ -15,16 +16,17 @@ HBASE_PORT=${HBASE_PORT:-2181}
 DISABLE_DEBUG=${DISABLE_DEBUG:-true}
 
 
-sed -i -r "s/(cluster.enable)=true/\1=${CLUSTER_ENABLE}/g" $TOMCAT_PATH/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
-sed -i -r "s/(cluster.zookeeper.address)=localhost/\1=${CLUSTER_ZOOKEEPER_ADDRESS}/g" $TOMCAT_PATH/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
-sed -i -r "s/(collector.tcpListenPort)=9994/\1=${COLLECTOR_TCP_PORT}/g" ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
-sed -i -r "s/(collector.udpStatListenPort)=9995/\1=${COLLECTOR_UDP_STAT_LISTEN_PORT}/g" ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
-sed -i -r "s/(collector.udpSpanListenPort)=9996/\1=${COLLECTOR_UDP_SPAN_LISTEN_PORT}/g" ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
-sed -i -r "s/(hbase.client.host)=localhost/\1=${HBASE_HOST}/g" ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/hbase.properties
-sed -i -r "s/(hbase.client.port)=2181/\1=${HBASE_PORT}/g" ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/hbase.properties
+sed -i -r -e "s/(cluster.enable)=true/\1=${CLUSTER_ENABLE}/g" \
+          -e "s/(cluster.zookeeper.address)=localhost/\1=${CLUSTER_ZOOKEEPER_ADDRESS}/g" \
+          -e "s/(collector.tcpListenPort)=9994/\1=${COLLECTOR_TCP_PORT}/g" \
+          -e "s/(collector.udpStatListenPort)=9995/\1=${COLLECTOR_UDP_STAT_LISTEN_PORT}/g" \
+          -e "s/(collector.udpSpanListenPort)=9996/\1=${COLLECTOR_UDP_SPAN_LISTEN_PORT}/g" ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
+
+sed -i -r -e "s/(hbase.client.host)=localhost/\1=${HBASE_HOST}/g" \
+          -e "s/(hbase.client.port)=2181/\1=${HBASE_PORT}/g" ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/hbase.properties
 
 if [ "$DISABLE_DEBUG" == "true" ]; then
-    sed -i 's/level value="DEBUG"/level value="INFO"/' /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/log4j.xml
+    sed -i 's/level value="DEBUG"/level value="INFO"/' ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/log4j.xml
 fi
 
-exec /usr/local/tomcat/bin/catalina.sh run
+exec ${TOMCAT_PATH}/bin/catalina.sh run
--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 set -e
-set -x
+
+[[ $DEBUG ]] && set -x
 
 CLUSTER_ENABLE=${CLUSTER_ENABLE:-false}
 CLUSTER_ZOOKEEPER_ADDRESS=${CLUSTER_ZOOKEEPER_ADDRESS:-127.0.0.1}
-
-COLLECTOR_TCP_PORT=${COLLECTOR_TCP_PORT:-9994}
-COLLECTOR_UDP_STAT_LISTEN_PORT=${COLLECTOR_UDP_STAT_LISTEN_PORT:-9995}
-COLLECTOR_UDP_SPAN_LISTEN_PORT=${COLLECTOR_UDP_SPAN_LISTEN_PORT:-9996}
+ADMIN_PASSWORD=${ADMIN_PASSWORD:-admin}
 
 HBASE_HOST=${HBASE_HOST:-127.0.0.1}
 HBASE_PORT=${HBASE_PORT:-2181}
 
 DISABLE_DEBUG=${DISABLE_DEBUG:-true}
 
+JDBC_DRIVER=${JDBC_DRIVER:-com.mysql.jdbc.Driver}
+JDBC_URL=${JDBC_URL:-jdbc:mysql://${MYSQL_HOST}:${MYSQL_PORT}/pinpoint?characterEncoding=UTF-8}
+JDBC_USERNAME=${MYSQL_USER:-admin}
+JDBC_PASSWORD=${MYSQL_PASS:-admin}
 
-sed -i -r "s/(cluster.enable)=true/\1=${CLUSTER_ENABLE}/g" $TOMCAT_PATH/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
-sed -i -r "s/(cluster.zookeeper.address)=localhost/\1=${CLUSTER_ZOOKEEPER_ADDRESS}/g" $TOMCAT_PATH/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
-sed -i -r "s/(collector.tcpListenPort)=9994/\1=${COLLECTOR_TCP_PORT}/g" ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
-sed -i -r "s/(collector.udpStatListenPort)=9995/\1=${COLLECTOR_UDP_STAT_LISTEN_PORT}/g" ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
-sed -i -r "s/(collector.udpSpanListenPort)=9996/\1=${COLLECTOR_UDP_SPAN_LISTEN_PORT}/g" ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/pinpoint-collector.properties
-sed -i -r "s/(hbase.client.host)=localhost/\1=${HBASE_HOST}/g" ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/hbase.properties
-sed -i -r "s/(hbase.client.port)=2181/\1=${HBASE_PORT}/g" ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/hbase.properties
+echo -e "
+jdbc.driverClassName=${JDBC_DRIVER}
+jdbc.url=${JDBC_URL}
+jdbc.username=${JDBC_USERNAME}
+jdbc.password=${JDBC_PASSWORD}
+" > ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/jdbc.properties
+
+
+
+sed -i -r -e "s/(cluster.enable)=true/\1=${CLUSTER_ENABLE}/g" \
+          -e "s/(cluster.zookeeper.address)=localhost/\1=${CLUSTER_ZOOKEEPER_ADDRESS}/g" \
+          -e "s/(admin.password)=admin/\1=${ADMIN_PASSWORD}/g" ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/pinpoint-web.properties
+
+sed -i -r -e "s/(hbase.client.host)=localhost/\1=${HBASE_HOST}/g" \
+          -e "s/(hbase.client.port)=2181/\1=${HBASE_PORT}/g" ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/hbase.properties
 
 if [ "$DISABLE_DEBUG" == "true" ]; then
-    sed -i 's/level value="DEBUG"/level value="INFO"/' /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/log4j.xml
+    sed -i -r 's/(level value)="DEBUG"/\1="INFO"/' ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/log4j.xml
 fi
 
-exec /usr/local/tomcat/bin/catalina.sh run
+exec ${TOMCAT_PATH}/bin/catalina.sh run
