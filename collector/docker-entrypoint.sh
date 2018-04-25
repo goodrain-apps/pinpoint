6c01143f0cb6f30f734ea19ee0d89a087ab02377
guox <guox@goodrain.com>
2018-04-25 12:21:16 +0800

    提供pingpoint监视自身 collector组件与web组件 的开关

--- collector/docker-entrypoint.sh
+++ collector/docker-entrypoint.sh
@@ -16,6 +16,12 @@ ZK_HOST=${HBASE_HOST:-127.0.0.1}
 
 DISABLE_DEBUG=${DISABLE_DEBUG:-true}
 
+# detect ENABLE_APM env
+if [ "$ENABLE_APM" == "true" ];then
+    COLLECTOR_IP=${COLLECTOR_IP:-127.0.0.1}
+    sed -i "2 a. /usr/local/tomcat/bin/pinpoint-agent.sh" /usr/local/tomcat/bin/catalina.sh
+    sed -i -r "s/(profiler.collector.ip)=.*/\1=${COLLECTOR_IP}/" /usr/local/pinpoint-agent/pinpoint.config
+fi
 
 sed -i -r -e "s/(cluster.enable)=true/\1=${CLUSTER_ENABLE}/g" \
           -e "s/(cluster.zookeeper.address)=localhost/\1=${CLUSTER_ZOOKEEPER_ADDRESS}/g" \
--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -25,7 +25,12 @@ jdbc.username=${JDBC_USERNAME}
 jdbc.password=${JDBC_PASSWORD}
 " > ${TOMCAT_PATH}/webapps/ROOT/WEB-INF/classes/jdbc.properties
 
-
+# detect ENABLE_APM env
+if [ "$ENABLE_APM" == "true" ];then
+    COLLECTOR_IP=${COLLECTOR_IP:-127.0.0.1}
+    sed -i "2 a. /usr/local/tomcat/bin/pinpoint-agent.sh" /usr/local/tomcat/bin/catalina.sh
+    sed -i -r "s/(profiler.collector.ip)=.*/\1=${COLLECTOR_IP}/" /usr/local/pinpoint-agent/pinpoint.config
+fi
 
 sed -i -r -e "s/(cluster.enable)=true/\1=${CLUSTER_ENABLE}/g" \
           -e "s/(cluster.zookeeper.address)=localhost/\1=${CLUSTER_ZOOKEEPER_ADDRESS}/g" \
